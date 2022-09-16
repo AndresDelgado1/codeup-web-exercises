f2a102b8626b27d75b07adc686fa5705840a02c7
Andres Delgado <andresdelgado335@gmail.com>
2022-09-16 16:06:16 -0500

    Finished exercises 3, 4, and 5

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -6,8 +6,33 @@
 //showMultiplicationTable(7);
 
 
-
+function randomNumber() {
+    for (let i = 0; i < 10; i++) {
+        var random = Math.floor((Math.random() * 180) + 21);
+        if (random % 2 === 0) {
+            console.log(random + " is even" )
+        } else {
+            console.log(random + " is odd")
+        }
+    }
 }
+randomNumber()
+
+
+//for(i = 1; i <= 9; i++){
+//    console.log(i.toString().repeat(i));
+//}
+
+
+
+//for(let i = 100; i >= 5; i-= 5){
+//    console.log(i)
+//}
+
+
+
+
+
 
 
 
