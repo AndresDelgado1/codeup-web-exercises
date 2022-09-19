e184de1567f6a6db2a06caf888088e5fbc907026
Andres Delgado <andresdelgado335@gmail.com>
2022-09-19 09:36:46 -0500

    Updated 'break and continue' exercise

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -1,14 +1,31 @@
+//function oddNumbers() {
+//    let userOddNumber = prompt("Pick an odd number between 1 and 50!")
+//    for (let i = 1; i <= 50; i += 2) {
+//        if (userOddNumber % 2 === 0) {
+//            alert('That is an even number. I said to pick an odd number!')
+//            break;
+//        } else if (i == userOddNumber) {
+//            continue;
+//        }
+//          console.log('Here is an odd number: ' + i)
+//      }
+//}
+//oddNumbers()
+
+
 function oddNumbers() {
-    let userOddNumber = prompt("Pick an odd number between 1 and 50!")
-    for (let i = 1; i <= 50; i += 2) {
-        if (userOddNumber % 2 === 0) {
-            alert('That is an even number. I said to pick an odd number!')
+    let userOddNumber;
+    for(i = 1;; i++){
+        userOddNumber = prompt("Pick an odd number between 1 and 50!");
+        if(userOddNumber % 2 === 1 && userOddNumber > 0 && userOddNumber < 50){
             break;
-        } else if(i == userOddNumber){
+        }
+    }
+    for(i = 1; i < 50; i += 2){
+        if (i === userOddNumber){
             continue;
         }
-            console.log('Here is an odd number: ' + i)
-
+        console.log('Here is an odd number: ' + i);
     }
 }
 oddNumbers()
