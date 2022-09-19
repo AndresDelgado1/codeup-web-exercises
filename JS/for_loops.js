2aecce25d777b49284831a7d7d7c9d3f992f25d4
Andres Delgado <andresdelgado335@gmail.com>
2022-09-18 20:10:05 -0500

    Updated syntax

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,8 +1,8 @@
-//function showMultiplicationTable(x){
-//    for(let i = 1; i <= 10; i++){
-//        console.log(x * i);
-//    }
-//}
+function showMultiplicationTable(x){
+    for(let i = 1; i <= 10; i++){
+        console.log(x * i);
+    }
+}
 //showMultiplicationTable(7);
 
 
@@ -16,18 +16,26 @@ function randomNumber() {
         }
     }
 }
-randomNumber()
+//randomNumber()
 
 
-//for(i = 1; i <= 9; i++){
-//    console.log(i.toString().repeat(i));
-//}
+function numberPyramid(){
+for(i = 1; i <= 9; i++) {
+    console.log(i.toString().repeat(i));
+}
+}
+//numberPyramid()
+
+
+function countDown(){
+for(let i = 100; i >= 5; i-= 5){
+    console.log(i)
+ }
+}
+//countDown()
 
 
 
-//for(let i = 100; i >= 5; i-= 5){
-//    console.log(i)
-//}
 
 
 
