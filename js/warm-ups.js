7ccc256c13429e27ce5a934f458f8a3cebd3f15c
Andres Delgado <andresdelgado335@gmail.com>
2022-09-29 09:30:56 -0500

    started warm up

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -98,8 +98,14 @@ function evenNumbers() {
 
 
 //Write a function that takes an array of objects and a string as arguments. Add a property with key ‘continent’ and //value equal to the string to each of the objects. Return the new array of objects. Don’t mutate the original array.
+// example input: [{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'
+// expected output: [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
+let cities = [{city: 'Tokyo', country: 'Japan'}, { city: 'Bangkok', country: 'Thailand'}]
+let where = 'Asia'
+function x (array, string){
+    return cities[0].push('continent:')
 
-
+}
 
 
 
