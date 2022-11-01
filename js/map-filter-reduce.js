f66f360a0d61c0e47c9975d6334f90e86f62b312
Andres Delgado <andresdelgado335@gmail.com>
2022-11-01 09:51:56 -0500

    finished exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -38,13 +38,16 @@ const users = [
 
 
 // Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+let filteredEmails = users.filter(e => e.languages.length >= 3);
 
 
 // Use .map to create an array of strings where each element is a user's email address
-let usersEmail = users.map((e) => e.email)
+let usersEmail = users.map(e => e.email)
 
 // Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use     the result to calculate the average.
-
+const totalYears = users.reduce((total, person) => {
+    return total + person.yearsOfExperience;
+}, 0);
 
 // Use .reduce to get the longest email from the list of users.
 let longestEmail = users.reduce((x, e) => x.email.length > e.email.length ? x : e).email
