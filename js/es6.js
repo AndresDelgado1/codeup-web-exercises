8db3fde0b4d69caa9f3e33a6445181263edb2f00
Andres Delgado <andresdelgado335@gmail.com>
2023-02-04 14:56:47 -0600

    practiced code

--- js/es6.js
+++ js/es6.js
@@ -71,6 +71,7 @@ users.forEach(function(user) {
 
   // TODO: rewrite the assignment below to use template strings ✅
   developers.push(`${name}'s email is ${email}, ${name} knows ${languages}`);
+
 });
 // TODO: Use `let` for the following variable ✅
 let list = '<ul>';
--- practice/js-practice.js
+++ practice/js-practice.js
@@ -1,6 +1,33 @@
-fetch( 'https://dog.ceo/api/breeds/image/random')
-    .then((response) => response.jason())
-    .then((data) => console.log(data))
-    .catch(error => console.log(error))
+// function fizzbuzz(){
+//     for(let i = 0; i <= 100; i ++ ){
+//         if(i % 3 === 0 && i % 5 === 0 ){
+//             console.log("fizzbuzz");
+//         } else if ( i % 3 === 0){
+//             console.log("fizz");
+//         } else if ( i % 5 === 0 ){
+//             console.log("buzz");
+//         } else{
+//             console.log(i);
+//         }
+//     }
+//
+// }
+// fizzbuzz()
+
+function foobar(){
+    for(let i = 17; i <= 53; i ++ ){
+        if(i % 2 === 0 && i % 5 === 0 ){
+            console.log("FooBar");
+        } else if ( i % 2 === 0){
+            console.log("Foo");
+        } else if ( i % 5 === 0 ){
+            console.log("Bar");
+        } else{
+            console.log(i);
+        }
+    }
+
+}
+foobar()
 
 
